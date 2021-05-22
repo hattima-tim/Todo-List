eeb8cb6ed8c666c178bbeb413396f6ce2517453f
ridwan <irfanridwan20@gmail.com>
2021-05-22 19:53:17 +0600

    added logic for project creation and project switching and some code improvements

--- src/index.js
+++ src/index.js
@@ -1,31 +1,56 @@
 import {tasks,createTask} from './applicationLogic';
-let projects=[{tasks}]
-let task=projects[0].tasks;
+let projects=[[]];
+let task;
+let defaultProject=document.querySelector('#default_project');
+defaultProject.setAttribute('data-index',`${0}`);
+defaultProject.addEventListener('click',(e)=>{
+    switchProject(e.target.dataset.index);
+    task=projects[e.target.dataset.index];
+    for(let i=0;i<task.length;i++){
+        display(task,i);
+    }
+})
 let table=document.querySelector('#table');
-function display(a){
+function display(task,index){
     let tableRow=document.createElement('tr');
     let titleInfo=document.createElement('td');
-    titleInfo.textContent=task[a].title;
+    titleInfo.textContent=task[index].title;
     let detailsInfo=document.createElement('td');
-    detailsInfo.textContent=task[a].details;
-    let closeButton=document.createElement('span');
-    closeButton.textContent='x';
+    detailsInfo.textContent=task[index].details;
     tableRow.appendChild(titleInfo);
     tableRow.appendChild(detailsInfo);
-    tableRow.appendChild(closeButton);
     table.appendChild(tableRow);
 }
-let a=0;
-let submitButton=document.querySelector('.submit');
-submitButton.addEventListener('click',()=>{
+let submitButton=document.querySelector('#submit');
+submitButton.setAttribute('data-index',`${0}`);
+submitButton.addEventListener('click',(e)=>{
     let title=`${form[0].value}`;
     let details=`${form[1].value}`;
-    task[a]=createTask(title,details);
-    display(a);
-    a++;
+    task=projects[e.target.dataset.index];
+    task.push(createTask(title,details));
+    display(task,task.length-1);
 })
-let addProject=document.querySelector('#add_project');
-addProject.addEventListener('click',()=>{
+let projectList=document.querySelector('#projects');
+let createNewProject=document.querySelector('#add_project');
+createNewProject.addEventListener('click',()=>{
+    projects.push([]);
     let projectName=prompt('Enter a name');
-    
+    let aElement=document.createElement('a');
+    aElement.textContent=`${projectName}`;
+    aElement.setAttribute('data-index',`${projects.length-1}`);
+    aElement.addEventListener('click',(e)=>{
+        switchProject(e.target.dataset.index);
+        task=projects[e.target.dataset.index];
+        for(let i=0;i<task.length;i++){
+            display(task,i);
+        }
+    });
+    projectList.appendChild(aElement);
+    switchProject(projects.length-1);
 })
+let switchProject=(index)=>{
+    while (table.firstChild) {
+        table.firstChild.remove();
+    }
+    submitButton.setAttribute('data-index',`${index}`);
+}

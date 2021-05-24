d6e4a90bbdc4fe82f1a4757b9e91dc245c70a6b3
ridwan <irfanridwan20@gmail.com>
2021-05-24 19:36:46 +0600

    removed unnecessary code,updated switchProject function,added project deletion logic,added project index updating function and some other simple changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import {tasks,createTask} from './applicationLogic';
+import {createTask} from './applicationLogic';
 let projects=[[]];
 let task;
 let defaultProject=document.querySelector('#default_project');
 defaultProject.setAttribute('data-index',`${0}`);
 defaultProject.addEventListener('click',(e)=>{
     switchProject(e.target.dataset.index);
-    task=projects[e.target.dataset.index];
-    for(let i=0;i<task.length;i++){
-        display(task,i);
-    }
 })
 let table=document.querySelector('#table');
 function display(task,index){
@@ -30,27 +26,53 @@ submitButton.addEventListener('click',(e)=>{
     task.push(createTask(title,details));
     display(task,task.length-1);
 })
-let projectList=document.querySelector('#projects');
+
+let updateIndexOfProjects=(index)=>{
+    let projectNameList=document.querySelectorAll('.project_name');
+    for (let i=index;i<projectNameList.length-1;i++){
+        let indexOfNextproject=Number(i)+1;
+        projectNameList[indexOfNextproject].setAttribute('data-index',`${i}`);
+    }
+}
+
+let switchProject=(index)=>{
+    while (table.firstChild) {
+        table.firstChild.remove();
+    }
+    task=projects[index];
+    for(let i=0;i<task.length;i++){
+        display(task,i);
+    }
+    submitButton.setAttribute('data-index',`${index}`);
+}
+
+let sidenav=document.querySelector('.sidenav');
 let createNewProject=document.querySelector('#add_project');
 createNewProject.addEventListener('click',()=>{
     projects.push([]);
     let projectName=prompt('Enter a name');
     let aElement=document.createElement('a');
     aElement.textContent=`${projectName}`;
+    aElement.classList.add('project_name');
     aElement.setAttribute('data-index',`${projects.length-1}`);
     aElement.addEventListener('click',(e)=>{
-        switchProject(e.target.dataset.index);
-        task=projects[e.target.dataset.index];
-        for(let i=0;i<task.length;i++){
-            display(task,i);
-        }
+        let indexOfClickedProject=e.target.dataset.index;
+        switchProject(indexOfClickedProject);
     });
-    projectList.appendChild(aElement);
+    let side_nav_project_name_container=document.createElement('div');
+    side_nav_project_name_container.classList.add('side_nav_project_name_holder');
+    let close=document.createElement('span');
+    close.textContent='x';
+    close.setAttribute('data-index',`${projects.length-1}`);
+    close.addEventListener('click',(e)=>{
+        let indexOfCurrentProject=e.target.previousElementSibling.dataset.index;
+        projects.splice(indexOfCurrentProject,1);
+        updateIndexOfProjects(indexOfCurrentProject);
+        switchProject(0);
+        e.target.parentNode.remove();
+    })
+    side_nav_project_name_container.appendChild(aElement);
+    side_nav_project_name_container.appendChild(close);
+    sidenav.appendChild(side_nav_project_name_container);
     switchProject(projects.length-1);
 })
-let switchProject=(index)=>{
-    while (table.firstChild) {
-        table.firstChild.remove();
-    }
-    submitButton.setAttribute('data-index',`${index}`);
-}

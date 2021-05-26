8d106804b6561dd5941a2c3ce09d94da6b3be55b
ridwan <irfanridwan20@gmail.com>
2021-05-26 20:49:51 +0600

    added a function for sorting tasks according to their importance

--- src/applicationLogic.js
+++ src/applicationLogic.js
@@ -1,6 +1,12 @@
-let createTask=(title,details,dueDate)=>{
-    return {title,details,dueDate}
+let createTask=(importance,title,details,dueDate)=>{
+    return {importance,title,details,dueDate}
+}
+let sortedTasks=(task)=>{
+    return task.sort(function (a,b){
+        return a.importance-b.importance;
+    })
 }
 export {
-    createTask
+    createTask,
+    sortedTasks
 }

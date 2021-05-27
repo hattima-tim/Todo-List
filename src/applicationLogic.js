08c31f833bc2ea846aa16b5a34bd8c5dacbdfa51
ridwan <irfanridwan20@gmail.com>
2021-05-27 20:02:23 +0600

    added a function for converting numeric values of importance dropdown of 'form' to human readable importance values

--- src/applicationLogic.js
+++ src/applicationLogic.js
@@ -1,12 +1,29 @@
-let createTask=(importance,title,details,dueDate)=>{
-    return {importance,title,details,dueDate}
+let createTask=(importance,title,description,dueDate)=>{
+
+    return {importance,title,description,dueDate}
 }
 let sortedTasks=(task)=>{
     return task.sort(function (a,b){
         return a.importance-b.importance;
     })
 }
+let create_human_readable_importance_vaule=(value)=>{
+    let coolImportanceValue;
+    switch(value){
+        case '1':
+            coolImportanceValue='High';
+            break;
+        case '2':
+            coolImportanceValue='medium';
+            break;
+        case '3':
+            coolImportanceValue='Low';
+            break;
+    }
+    return coolImportanceValue;
+}
 export {
     createTask,
-    sortedTasks
+    sortedTasks,
+    create_human_readable_importance_vaule
 }

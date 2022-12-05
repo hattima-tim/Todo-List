3ee75fae2a8845485bf04d6e00e3b1c11e66a066
hattima-tim <irfanridwan20@gmail.com>
2022-12-05 12:22:53 +0000

    initialize firebase

--- src/index.js
+++ src/index.js
@@ -1,119 +1,170 @@
-import {createTask,showAllTasksOfCurrentProject,showAllCurrentProjects} from './applicationLogic';
-import {createDomStructurForProject} from './domStructure';
-let projects=JSON.parse(localStorage.getItem('projectArray')) || [[]];
+import { initializeApp } from "firebase/app";
+
+import {
+  createTask,
+  showAllTasksOfCurrentProject,
+  showAllCurrentProjects,
+} from "./applicationLogic";
+import { createDomStructurForProject } from "./domStructure";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVcyTrhhQce-iGjNGS63F5Cayr5ZuDizA",
+  authDomain: "todo-list-7046d.firebaseapp.com",
+  projectId: "todo-list-7046d",
+  storageBucket: "todo-list-7046d.appspot.com",
+  messagingSenderId: "1089066977716",
+  appId: "1:1089066977716:web:99030fdd3bbc5583908ae1",
+  measurementId: "G-J27P39SHDS",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+let projects = JSON.parse(localStorage.getItem("projectArray")) || [[]];
 let currentProjectTaskList;
 let projectName;
-let projectNameArray=JSON.parse(localStorage.getItem('projectNameArray')) || ["home"];
-let projectHeader=document.querySelector('#project_header');
+let projectNameArray = JSON.parse(localStorage.getItem("projectNameArray")) || [
+  "home",
+];
+let projectHeader = document.querySelector("#project_header");
 
-let taskCompletionCounterDOM=document.querySelector('#total_task_completed');
-let taskCompletionCounter=JSON.parse(localStorage.getItem('totalCompletedTask')) || 0;
-localStorage.setItem('totalCompletedTask',JSON.stringify(taskCompletionCounter));
-taskCompletionCounterDOM.textContent=`Completed (${taskCompletionCounter})`;
+let taskCompletionCounterDOM = document.querySelector("#total_task_completed");
+let taskCompletionCounter =
+  JSON.parse(localStorage.getItem("totalCompletedTask")) || 0;
+localStorage.setItem(
+  "totalCompletedTask",
+  JSON.stringify(taskCompletionCounter)
+);
+taskCompletionCounterDOM.textContent = `Completed (${taskCompletionCounter})`;
 
-currentProjectTaskList=projects[0];
+currentProjectTaskList = projects[0];
 showAllTasksOfCurrentProject(currentProjectTaskList);
 
-let formContainer=document.querySelector('#form_container');
-let addTaskButton=document.querySelector('#add_task_button');
-addTaskButton.addEventListener('click',()=>{
-    formContainer.style.display='flex';
-    formContainer.style.justifyContent='center';
-    formContainer.style.alignItems='center';
-})
+let formContainer = document.querySelector("#form_container");
+let addTaskButton = document.querySelector("#add_task_button");
+addTaskButton.addEventListener("click", () => {
+  formContainer.style.display = "flex";
+  formContainer.style.justifyContent = "center";
+  formContainer.style.alignItems = "center";
+});
 
-let formSubmitButton=document.querySelector('#form_submit_button');
-let importanceDropdown=document.querySelector('#importance');
-formSubmitButton.setAttribute('data-index',`${0}`);
-formSubmitButton.addEventListener('click',(e)=>{
-    formContainer.style.display='none';
-    let taskTitle=`${form[0].value}`;
-    let taskDescription=`${form[1].value}`;
-    let taskImportance=`${importanceDropdown.value}`;
-    let taskDueDate=`${form[3].value}`;
-    
-    let newTask=createTask(taskImportance,taskTitle,taskDescription,taskDueDate);
-    currentProjectTaskList=projects[e.target.dataset.index];
-    currentProjectTaskList.push(newTask);
-    
-    localStorage.setItem('projectArray',JSON.stringify(projects));
+let formSubmitButton = document.querySelector("#form_submit_button");
+let importanceDropdown = document.querySelector("#importance");
+formSubmitButton.setAttribute("data-index", `${0}`);
+formSubmitButton.addEventListener("click", (e) => {
+  formContainer.style.display = "none";
+  let taskTitle = `${form[0].value}`;
+  let taskDescription = `${form[1].value}`;
+  let taskImportance = `${importanceDropdown.value}`;
+  let taskDueDate = `${form[3].value}`;
 
-    showAllTasksOfCurrentProject(currentProjectTaskList);
-    form.reset();
-})
+  let newTask = createTask(
+    taskImportance,
+    taskTitle,
+    taskDescription,
+    taskDueDate
+  );
+  currentProjectTaskList = projects[e.target.dataset.index];
+  currentProjectTaskList.push(newTask);
 
-let formCloseButton=document.querySelector('#form_close_button');
-formCloseButton.addEventListener('click',()=>{
-    formContainer.style.display='none';
-})
+  localStorage.setItem("projectArray", JSON.stringify(projects));
 
-let detailsModal=document.querySelector('#details_modal');
-let detailsModalContent=document.querySelector('#datails_modal_content');
-let detailsModalCloseButton=document.querySelector('#details_modal_close_button');
-detailsModalCloseButton.addEventListener('click',()=>{
-    for (let i=0;i<3;i++) {
-        detailsModalContent.lastChild.remove();
-    }
-    detailsModal.style.display='none';
-})
+  showAllTasksOfCurrentProject(currentProjectTaskList);
+  form.reset();
+});
 
-let containerOfFormForEditingTask=document.querySelector('#container_of_form_for_editing_task');
-let formForEditingTask=document.querySelector('#form_for_editing_task');
-let task_importance_dropdown_of_form_for_task_editing=document.querySelector('#task_importance_dropdown_of_form_for_task_editing');
-let submitButtonForEditingTask=document.querySelector('#submit_button_of_form_for_editing_task');
-submitButtonForEditingTask.addEventListener('click',(e)=>{
-    containerOfFormForEditingTask.style.display='none';
-    let taskTitle=formForEditingTask[0].value;
-    let taskDescription=formForEditingTask[1].value;
-    let taskImportance=task_importance_dropdown_of_form_for_task_editing.value;
-    let taskDueDate=formForEditingTask[3].value;
-    
-    let newTask=createTask(taskImportance,taskTitle,taskDescription,taskDueDate);
-    let index_of_the_task_which_need_to_be_Edited=e.target.dataset.index;
-    currentProjectTaskList.splice(index_of_the_task_which_need_to_be_Edited,1,newTask);
+let formCloseButton = document.querySelector("#form_close_button");
+formCloseButton.addEventListener("click", () => {
+  formContainer.style.display = "none";
+});
 
-    localStorage.setItem('projectArray',JSON.stringify(projects));
+let detailsModal = document.querySelector("#details_modal");
+let detailsModalContent = document.querySelector("#datails_modal_content");
+let detailsModalCloseButton = document.querySelector(
+  "#details_modal_close_button"
+);
+detailsModalCloseButton.addEventListener("click", () => {
+  for (let i = 0; i < 3; i++) {
+    detailsModalContent.lastChild.remove();
+  }
+  detailsModal.style.display = "none";
+});
 
-    showAllTasksOfCurrentProject(currentProjectTaskList);
-})
+let containerOfFormForEditingTask = document.querySelector(
+  "#container_of_form_for_editing_task"
+);
+let formForEditingTask = document.querySelector("#form_for_editing_task");
+let task_importance_dropdown_of_form_for_task_editing = document.querySelector(
+  "#task_importance_dropdown_of_form_for_task_editing"
+);
+let submitButtonForEditingTask = document.querySelector(
+  "#submit_button_of_form_for_editing_task"
+);
+submitButtonForEditingTask.addEventListener("click", (e) => {
+  containerOfFormForEditingTask.style.display = "none";
+  let taskTitle = formForEditingTask[0].value;
+  let taskDescription = formForEditingTask[1].value;
+  let taskImportance = task_importance_dropdown_of_form_for_task_editing.value;
+  let taskDueDate = formForEditingTask[3].value;
 
-let closeButtonOfFormForEditingTask=document.querySelector('#close_button_of_form_for_editing_task');
-closeButtonOfFormForEditingTask.addEventListener('click',()=>{
-    containerOfFormForEditingTask.style.display='none';
-})
+  let newTask = createTask(
+    taskImportance,
+    taskTitle,
+    taskDescription,
+    taskDueDate
+  );
+  let index_of_the_task_which_need_to_be_Edited = e.target.dataset.index;
+  currentProjectTaskList.splice(
+    index_of_the_task_which_need_to_be_Edited,
+    1,
+    newTask
+  );
 
-let createNewProjectButton=document.querySelector('#add_project');
-createNewProjectButton.addEventListener('click',()=>{
-    projects.push([]);
-    projectName=prompt('Enter a Name');
-    projectNameArray.push(projectName);
-    
-    localStorage.setItem('projectArray',JSON.stringify(projects));
-    localStorage.setItem('projectNameArray',JSON.stringify(projectNameArray));
-    
-    let index=projects.length-1;
-    createDomStructurForProject(switchProject,createNewProjectButton,projectName,index)
-})
+  localStorage.setItem("projectArray", JSON.stringify(projects));
 
-let switchProject=(index,projectName)=>{
-    currentProjectTaskList=projects[index];
-    showAllTasksOfCurrentProject(currentProjectTaskList);
-    formSubmitButton.setAttribute('data-index',`${index}`);
-    projectHeader.textContent=`${projectName}`
-}
+  showAllTasksOfCurrentProject(currentProjectTaskList);
+});
 
-let home=document.querySelector('#home');
-home.setAttribute('data-index',`${0}`);
-home.addEventListener('click',(e)=>{
-    switchProject(e.target.dataset.index,'Home');
-})
+let closeButtonOfFormForEditingTask = document.querySelector(
+  "#close_button_of_form_for_editing_task"
+);
+closeButtonOfFormForEditingTask.addEventListener("click", () => {
+  containerOfFormForEditingTask.style.display = "none";
+});
+
+let createNewProjectButton = document.querySelector("#add_project");
+createNewProjectButton.addEventListener("click", () => {
+  projects.push([]);
+  projectName = prompt("Enter a Name");
+  projectNameArray.push(projectName);
+
+  localStorage.setItem("projectArray", JSON.stringify(projects));
+  localStorage.setItem("projectNameArray", JSON.stringify(projectNameArray));
+
+  let index = projects.length - 1;
+  createDomStructurForProject(
+    switchProject,
+    createNewProjectButton,
+    projectName,
+    index
+  );
+});
+
+let switchProject = (index, projectName) => {
+  currentProjectTaskList = projects[index];
+  showAllTasksOfCurrentProject(currentProjectTaskList);
+  formSubmitButton.setAttribute("data-index", `${index}`);
+  projectHeader.textContent = `${projectName}`;
+};
+
+let home = document.querySelector("#home");
+home.setAttribute("data-index", `${0}`);
+home.addEventListener("click", (e) => {
+  switchProject(e.target.dataset.index, "Home");
+});
 
 showAllCurrentProjects();
 
-switchProject(0,'Home');
+switchProject(0, "Home");
 
-export {
-    projects,
-    projectNameArray,
-    switchProject
-}
+export { projects, projectNameArray, switchProject };

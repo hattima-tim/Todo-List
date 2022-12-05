5a68f3cf5a6430ce998d00dc79203a68ab1134fc
hattima-tim <irfanridwan20@gmail.com>
2022-12-05 17:14:10 +0000

    create firebaseLogic file

---
+++ src/firebaseLogic.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDVcyTrhhQce-iGjNGS63F5Cayr5ZuDizA",
+  authDomain: "todo-list-7046d.firebaseapp.com",
+  projectId: "todo-list-7046d",
+  storageBucket: "todo-list-7046d.appspot.com",
+  messagingSenderId: "1089066977716",
+  appId: "1:1089066977716:web:99030fdd3bbc5583908ae1",
+  measurementId: "G-J27P39SHDS",
+};
+ 
+
+export {firebaseConfig}

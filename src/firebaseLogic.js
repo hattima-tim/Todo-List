d707ec7f051ebc31ca8fca648e034102042efd2e
hattima-tim <irfanridwan20@gmail.com>
2022-12-06 11:07:18 +0000

    add functionality for handling auth state change

--- src/firebaseLogic.js
+++ src/firebaseLogic.js
@@ -27,8 +27,48 @@ function signOutUser() {
   signOut(getAuth());
 }
 
+// Returns the signed-in user's profile Pic URL.
+function getProfilePicUrl() {
+  return (
+    getAuth().currentUser.photoURL ||
+    "https://res.cloudinary.com/du3oueesv/image/upload/v1670301075/profile_placeholder_phbctd.png"
+  );
+}
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+  const userInfo = document.querySelector(".user_info");
+  const userPicElement = document.querySelector(".user_pic");
+  const signInArea = document.querySelector(".sign_in");
+
+  if (user) {
+    // User is signed in!
+    // Get the signed-in user's profile pic and name.
+    const profilePicUrl = getProfilePicUrl();
+    // Set the user's profile pic and name.
+    userPicElement.src = `${profilePicUrl}`;
+    // Show user's profile and sign-out button.
+    userInfo.style.display = "block";
+    // Hide sign-in button.
+    signInArea.style.display = "none";
+  } else {
+    // User is signed out!
+    // Hide user's profile and sign-out button.
+    userInfo.style.display = "none";
+    // Show sign-in button.
+    signInArea.style.display = "block";
+  }
+}
+
+// Initialize firebase auth
+function initFirebaseAuth() {
+  // Listen to auth state changes.
+  onAuthStateChanged(getAuth(), authStateObserver);
+}
+
 export {
   firebaseConfig,
   signIn,
   signOutUser,
+  initFirebaseAuth,
 };
